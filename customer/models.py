4ecc9eea113b61262502231a105450dcd3e0c6db
Korawan <korawan.ta@ku.th>
2024-04-02 04:10:14 +0700

    Add customer

--- customer/models.py
+++ customer/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from user.models import User
 from route.models import AddRoutes
 from car.models import Car
-from car.models import Seat
+
 import uuid
 #import random
 #import string
 
-
 STATUS = [("unpaid", "ยังไม่ชำระเงิน"), ("pending", "รอการตรวจสอบ"), ("paid", "ชำระเงินสำเร็จ")]
+
 # Create your models here.
 class Ticket(models.Model):
     user = models.ForeignKey(
@@ -16,14 +16,16 @@ class Ticket(models.Model):
     add_route = models.ForeignKey(
         AddRoutes, on_delete=models.CASCADE, null=True, blank=True)
     car = models.ForeignKey(
-        Car, on_delete=models.CASCADE, null=True, blank=True, related_name='tickets')
-    seat = models.ForeignKey(
+        Car, on_delete=models.CASCADE, null=True, blank=True, related_name='cars')
+    '''seat = models.ForeignKey(
         Seat, on_delete=models.CASCADE, null=True, blank=True, related_name='tickets')
-    img = models.ImageField(upload_to='tickets/', null=True, blank=True) 
+    img = models.ImageField(upload_to='tickets/', null=True, blank=True)''' 
     status = models.CharField(max_length=20, choices=STATUS, default="unpaid")
-    ticket_no =  models.CharField(max_length=20, default="")  
+    #ticket_no =  models.CharField(max_length=20, default="")  
+    
+    
 
-    def __str__(self):
+    '''def __str__(self):
         return self.ticket_no
     
     def save(self, *args, **kwargs):
@@ -32,7 +34,7 @@ class Ticket(models.Model):
             self.ticket_no = str(uuid.uuid4()).replace("-", "")[:20]  
         super().save(*args, **kwargs)
 
-    '''def save(self, *args, **kwargs):
+     def save(self, *args, **kwargs):
         if not self.ticket_no:
             self.ticket_no = self.generate_ticket_no()
         super().save(*args, **kwargs)
